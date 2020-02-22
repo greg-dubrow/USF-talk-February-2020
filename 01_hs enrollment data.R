4faa50209c7bc6ca4cdc96eb06f506c6f4de456d
Greg Dubrow <dubrowg@gmail.com>
2020-02-21 16:28:53 -0800

    data cleaning & ppt

--- 01_hs enrollment data.R
+++ 01_hs enrollment data.R
@@ -4,31 +4,84 @@ library(rCAEDDATA)
 library(ipeds)
 library(rsfsu)
 library(readr)
+library(janitor)
 
+# data files from https://www.cde.ca.gov/ds/sd/sd/
 
+## from rCAEDDATA package (dl from github
+ # only goes to 1516 - need 1617 and 1718 by school, eth, gender grads, uc grads
 
-## only goes to 1516 - need 1617 and 1718 by school, eth, gender grads, uc grads
+cahsgrad93to16 <- readRDS(file = "Data/cahsgrad93to16.rds") %>%
+	arrange(CDS_CODE, YEAR, ETHNIC, GENDER) %>%
+	mutate(ETHNIC = as.character(ETHNIC))
 
-# on home mac
-load("~/Data/USF-talk-February-2020/data/cahsgrad93to16.rda")
-# on work desktop
-load("C:/Data/research projects/USF-talk-February-2020/data/cahsgrad93to16.rda")
-glimpse(graduates)
+glimpse(cahsgrad93to16)
 
-graduates <- graduates %>%
+cahsgrad93to16 <- cahsgrad93to16 %>%
 	arrange(CDS_CODE, YEAR, ETHNIC, GENDER)
 
-graduates %>%
+cahsgrad93to16  %>%
 	count(YEAR) %>%
 	view()
 
 
 # gradall1617.txt has same structure as gradutes
 # graduates1718.xlsx is different, needs work to get to same structure
-gradall_1617 <- read_delim("data/gradall_1617.txt",
-													 "\t", escape_double = FALSE, trim_ws = TRUE)
-View(gradall_1617)
-glimpse(gradall_1617)
+cahsgrad17 <- read_delim("data/gradall_1617.txt",
+													 "\t", escape_double = FALSE, trim_ws = TRUE) %>%
+	mutate(YEAR = factor(YEAR)) %>%
+	mutate(ETHNIC = as.character(ETHNIC)) %>%
+	mutate(GRADS = as.integer(GRADS)) %>%
+	mutate(UC_GRADS = as.integer(UC_GRADS))
+
+glimpse(cahsgrad17)
+
+cahsgrad17 %>%
+	count(YEAR)
+
+cahsgrad93to17 <- rbind(cahsgrad93to16, cahsgrad17) %>%
+	arrange(CDS_CODE, YEAR, ETHNIC, GENDER)
+glimpse(cahsgrad93to17)
+
+cahsgrad93to17 %>%
+	count(YEAR) %>%
+	view()
+
+cahsgrad93to17_tot <- cahsgrad93to17 %>%
+	group_by(YEAR) %>%
+	summarise(total_grads = sum(GRADS),
+						Yes = sum(UC_GRADS),
+						No = total_grads - Yes) %>%
+	ungroup()
+glimpse(cahsgrad93to17_tot)
+
+# file structure https://www.cde.ca.gov/ds/sd/sd/fsacgr.asp
+# return a df with year, total grads and uc elig yes/no
+cahsgrad18 <- read.delim("C:/Data/research projects/USF-talk-February-2020/data/cahsgrad18.txt",
+												 stringsAsFactors=FALSE) %>%
+	clean_names() %>%
+	filter(reporting_category == "TA") %>%
+	filter(aggregate_level == "S") %>%
+	filter(dass == "All") %>%
+	filter(charter_school == "All") %>%
+	filter(school_name != "Nonpublic, Nonsectarian Schools") %>%
+	filter(school_name != "District Office") %>%
+	mutate(YEAR = "1718") %>%
+	mutate(YEAR = factor(YEAR)) %>%
+	mutate_at(vars(ends_with("_code")), as.character) %>%
+	mutate(county_code = ifelse(nchar(county_code) == 1, str_pad(county_code, 2, "left", "0"), county_code)) %>%
+	mutate(CDS_CODE = paste(county_code, district_code, school_code, sep = "")) %>%
+	mutate(GRADS = as.integer(ifelse(regular_hs_diploma_graduates_count == "*",
+																	 0, regular_hs_diploma_graduates_count))) %>%
+	mutate(UC_GRADS = as.integer(ifelse(met_uc_csu_grad_req_s_count == "*",
+																		 0, met_uc_csu_grad_req_s_count))) %>%
+	select(CDS_CODE, GRADS, UC_GRADS, YEAR) %>%
+	group_by(YEAR) %>%
+	summarise(total_grads = sum(GRADS),
+							Yes = sum(UC_GRADS),
+							No = total_grads - Yes)
+
+glimpse(cahsgrad18)
 
 
 
@@ -37,10 +90,9 @@ glimpse(gradall_1617)
 
 
 
-
-
-
-graduates %>%
+## charts
+# total by uc/csu elig & not
+cahsgrad93to17 %>%
 	group_by(YEAR) %>%
 	summarise(total_grads = sum(GRADS),
 						Yes = sum(UC_GRADS),
--- README.md
+++ README.md
@@ -9,3 +9,20 @@ Slides for invited talk at USF for campus forum. PEr emails with Angie Davis & t
 Use data from CA Dept Ed, IPEDS & other sources
 
 slides in ppt
+
+
+USF colors
+yellow 253 187 48  Hex = FDBB30
+green   0 84 60 hex = 00543C
+grey 145 145 148 hex = 919194
+
+secondary 
+black 
+white 
+
+palette 
+yellow 253 187 48 FDBB30
+orange 235 130 30 EB821E
+terra cotta 205 84 44 CD542C
+saddle tan 222 213 180 DED5B4
+plum 168 34 142 A8228E
